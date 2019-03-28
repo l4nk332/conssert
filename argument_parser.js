cfaa5abcfc1ae4fefefe9b2ac0eb2b5f2c817402
Ian Jabour <l4nk332@gmail.com>
2019-03-28 09:47:49 -0700

    Improve logic for arg parser to be immutable and handle flags in long and short forms.

--- argument_parser.js
+++ argument_parser.js
@@ -1,39 +1,47 @@
+function isArg(arg, lookup) {
+  return lookup.hasOwnProperty(arg)
+}
+
 function parseArgs(rawArgs) {
   const ARG_MAP = {
     LOCAL_DEV: {name: 'LOCAL_DEV'},
-    PORT: {name: 'PORT', takesValue: true},
-    IGNORE: {name: 'IGNORE', takesValue: true},
+    PORT: {name: 'PORT', singleValue: true},
+    IGNORE: {name: 'IGNORE', multiValue: true},
     HELP: {name: 'HELP'}
   }
 
   const ARG_LOOKUP = {
     '--local-dev': ARG_MAP.LOCAL_DEV,
-    '-p': ARG_MAP.PORT,
+    '-l': ARG_MAP.LOCAL_DEV,
     '--port': ARG_MAP.PORT,
+    '-p': ARG_MAP.PORT,
+    '--ignore': ARG_MAP.IGNORE,
     '-i': ARG_MAP.IGNORE,
-    '--ignore': ARG_MAP.IGNORE
+    '--help': ARG_MAP.HELP,
+    '-h': ARG_MAP.HELP
   }
 
-  const copiedRawArgs = [...rawArgs]
+  return [...rawArgs].reduce((collected, rawArg) => {
+    if (!isArg(rawArg, ARG_LOOKUP) && !collected.length) return collected
 
-  function isLongForm(param) {
-    return param.startsWith('--')
-  }
+    const lastArgCollection = collected.slice(-1)[0]
+    const restArgCollection = collected.slice(0, -1)
 
-  return copiedRawArgs.reduce((parsed, rawArg, idx, srcArr) => {
-    if (isLongForm(rawArg)) {
-      const [arg, value = true] = rawArg.split('=')
-      if (ARG_LOOKUP.hasOwnProperty(arg)) {
-        parsed[ARG_LOOKUP[arg].name] = value
-      }
-    } else if (ARG_LOOKUP.hasOwnProperty(rawArg)) {
-      const parsedArg = ARG_LOOKUP[rawArg]
-      const value = parsedArg.takesValue ? srcArr.splice(idx + 1, 1)[0] : true
-      parsed[parsedArg.name] = value
-    }
-
-    return parsed
-  }, {})
+    return (
+      isArg(rawArg, ARG_LOOKUP)
+        ? [...collected, [rawArg]]
+        : [...restArgCollection, [...lastArgCollection, rawArg]]
+    )
+  }, []).reduce((argStruct, [arg, ...values]) => ({
+    ...argStruct,
+    [ARG_LOOKUP[arg].name]: (
+      values.length === 0
+        ? true
+        : values.length === 1
+          ? values[0]
+          : values
+    )
+  }), {})
 }
 
 module.exports = parseArgs
